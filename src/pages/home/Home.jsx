5ee00998967d68666fd216be19941e7acf825e57
icaro-bezerra <icaro.bezerra@sptech.school>
2024-04-25 22:41:13 -0300

    estrutura

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+// Importa o arquivo de estilos CSS específico para este componente
+import styles from "./Home.module.css";
+
+const Home = () => {
+    return (
+        <div className={styles.container}>
+            <h1>Home</h1>
+        </div>
+    );
+}
+
+export default Home;
