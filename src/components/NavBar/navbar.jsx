412cb445e224d845d4d5fc8ba411c541e18300fd
miguelsilvag <miguel.silva@sptech.school>
2024-04-27 16:15:19 -0300

    Inicialização das telas

---
+++ src/components/NavBar/navbar.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom'; 
+import './navbar.css';
+import logo from '../../utils/assets/logo2.jpg';
+
+function Componente() {
+  const navigate = useNavigate();
+
+  const goToHome = () => {
+    navigate('/home'); 
+  };
+
+  const goToParaVoce = () => {
+    navigate('/para-voce'); 
+  };
+
+  const goToParaEspecialistas = () => {
+    navigate('/para-especialistas'); 
+  };
+
+  const goToLogin = () => {
+    navigate('/login'); 
+  };
+
+  return (
+    <div className="custom-navbar">
+      <div className="navbar-logo">
+        <img src={logo} alt="Logo" className="logo" />
+      </div>
+      <div className="navbar-menu"></div>
+      <div className="navbar-links">
+        <span className="navbar-link" onClick={goToHome}>Home</span>
+        <span className="navbar-link" onClick={goToParaVoce}>Para você</span>
+        <span className="navbar-link" onClick={goToParaEspecialistas}>Para Especialistas</span>
+        <div className="navbar-login">
+          <div className="login-container" onClick={goToLogin}>
+            <div className="login-button"><span className="login-text">Login</span></div>
+          </div>
+        </div>
+      </div>
+      <div className="navbar-extra"></div>
+      <div className="navbar-footer"></div>
+    </div>
+  );
+}
+
+export default Componente;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,34 @@
 import React from "react";
-// Importa o arquivo de estilos CSS específico para este componente
+import { useNavigate } from "react-router-dom";
 import styles from "./Home.module.css";
+import NavBar from "../../components/NavBar/navbar"; 
+import Logo from "../../utils/assets/logo2.jpg";
+import imgPrincipal from "../../utils/assets/fundo.svg";
+import imgEstrelas from "../../utils/assets/v767_27.png";
+
 
 const Home = () => {
+    const navigate = useNavigate(); 
+
+    const handleConversarClick = () => {
+        navigate("/pagina-de-conversa");
+    };
+
     return (
-        <div className={styles.container}>
-            <h1>Home</h1>
+        <div className={styles.background}>
+            <div className={styles.starBackground}>
+                <img src={imgEstrelas} alt="Estrelas" className={styles.starImage} />
+            </div>
+            <img src={imgPrincipal} alt="Imagem de fundo" className={styles.backgroundImage} />
+            <NavBar logo={Logo} />
+            <div className={styles.container}>
+                <h1>
+                    Onde há saúde Mental, <span> Há paz</span>
+                </h1>
+                <p>Prepare-se hoje para o amanhã e descubra a tranquilidade de cuidar da sua mente conosco.</p>
+                
+                <p onClick={handleConversarClick} className={styles.conversarLink}>Conversar</p>
+            </div>
         </div>
     );
 }
