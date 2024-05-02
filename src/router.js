ccc6d52bb3d4ab4271d9af2cfa86f974633db6fd
miguelsilvag <miguel.silva@sptech.school>
2024-05-02 20:05:47 -0300

    adição tela de erro

--- src/router.js
+++ src/router.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
+import NotFound from "./pages/home/notFound/notFound";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function Router() {
     return (
         <>
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Home />} />
-
+                    <Route path="*" element={<NotFound />} />
                 </Routes>
             </BrowserRouter>
         </>
