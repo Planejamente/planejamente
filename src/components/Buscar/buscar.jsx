93998eaf07f3777fa377500d2c3b77cfd1aca3fa
Vitor-R-Silva <vitor.rsilva@sptech.school>
2024-05-06 12:38:48 -0300

    Subindo botão de busca

--- src/components/Buscar/buscar.jsx
+++ src/components/Buscar/buscar.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import "./buscar.module.css";
+import styles from "./buscar.module.css";
+import SearchIcon from '../../utils/assets/search.png'
 
 const Buscar = () => {
     return (
         <div>
-            <button>Buscar</button>
+            <button className={styles['bntBuscar']}>Buscar
+                <img className={styles['searchIcon']} src={SearchIcon} alt="Search Icon" />
+            </button>
         </div>
     );
 };
