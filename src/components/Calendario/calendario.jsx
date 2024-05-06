c612e52bf7c4d9c5980155abc9a860f3ac36980e
Vitor-R-Silva <vitor.rsilva@sptech.school>
2024-05-06 17:01:48 -0300

    Subindo estilização do calendário de agendamento

--- src/components/Calendario/calendario.jsx
+++ src/components/Calendario/calendario.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Calendar from "react-calendar";
 import 'react-calendar/dist/Calendar.css';
+import "./calendario.css";
 
 const Calendario = () => {
     return (
