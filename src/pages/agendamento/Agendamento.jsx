9bc184de626b4c307fe7053d44e63e6a52899773
Vitor-R-Silva <vitor.rsilva@sptech.school>
2024-05-07 16:36:35 -0300

    Subindo componente de time picker e ajustes no footer, agendamento e responsividade no login

--- src/pages/agendamento/Agendamento.jsx
+++ src/pages/agendamento/Agendamento.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import Calendario from "../../components/Calendario/calendario";
-import Footer from "../../components/Footer/footer";
 import Buscar from "../../components/Buscar/buscar";
 import styles from "./Agendamento.module.css";
+import TimePicker from "../../components/TimePicker/timepicker";
+import Footer from "../../components/Footer/footer";
 
 const Agendamento = () => {
 
     return (
-        <content>
-            <p className={styles['title']}>Escolha o melhor dia e horário para você</p>
-            <Calendario />
-            <Buscar />
+        <div>
+            <h1 className={styles['title']}>Escolha o melhor dia e horário para você</h1>
+            <div className={styles['inputs']}>
+                <Calendario />
+                <TimePicker />
+            </div>
+            <div className={styles['bntBusca']}>
+                <Buscar />
+            </div>
             <Footer />
-        </content>
+        </div>
     );
 };
 
