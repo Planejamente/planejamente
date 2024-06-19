97f59bbb797247d5d078ce6d52c4d041d8d06d72
icaro-bezerra <icaro.bezerra@sptech.school>
2024-06-18 23:09:01 -0300

    autoCommit

--- src/components/NavBar/navbar.jsx
+++ src/components/NavBar/navbar.jsx
@@ -1,18 +1,25 @@
 import React from "react";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "./navbar.css";
 import logo from "../../utils/assets/logo2.jpg";
 
 function Componente() {
   const navigate = useNavigate();
-  const location = useLocation();
+
+  const goToHome = () => {
+    navigate("/home");
+  };
+
+  const goToParaVoce = () => {
+    navigate("/para-voce");
+  };
+
+  const goToParaEspecialistas = () => {
+    navigate("/para-especialistas");
+  };
 
   const goToLogin = () => {
     navigate("/login");
-  };
-
-  const goToHomePage = () => {
-    navigate("/");
   };
 
   const scrollToSection = (sectionId) => {
@@ -20,36 +27,37 @@ function Componente() {
     if (section) {
       window.scrollTo({
         behavior: "smooth",
-        top: section.offsetTop,
+        top: section.offsetTop
       });
-    }
-  };
-
-  const isHomePage = location.pathname === "/";
+  }
+};
 
   return (
     <div className="custom-navbar">
-      <div className="navbar-logo" onClick={goToHomePage}>
+      <div className="navbar-logo">
         <img src={logo} alt="Logo" className="logo" />
       </div>
+      <div className="navbar-menu"></div>
       <div className="navbar-links">
-        {isHomePage && (
-          <>
-            <span className="navbar-link-home" onClick={() => scrollToSection("sectionHome")}>
-              Home
-            </span>
-            <span className="navbar-link-vc" onClick={() => scrollToSection("sectionParaVoce")}>
-              Para você
-            </span>
-            <span className="navbar-link-espec" onClick={() => scrollToSection("sectionEspcialista")}>
-              Para Especialistas
-            </span>
-          </>
-        )}
+        <span className="navbar-link" onClick={() => scrollToSection("sectionHome")}>
+          Home
+        </span>
+        <span className="navbar-link" onClick={() => scrollToSection("sectionParaVoce")}>
+          Para você
+        </span>
+        <span className="navbar-link" onClick={() => scrollToSection("sectionEspcialista")}>
+          Para Especialistas
+        </span>
+        <div className="navbar-login">
+          <div className="login-container" onClick={goToLogin}>
+            <div className="login-button">
+              <span className="login-text">Login</span>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="login-button">
-        <span onClick={goToLogin}>Login</span>
-      </div>
+      <div className="navbar-extra"></div>
+      <div className="navbar-footer"></div>
     </div>
   );
 }
--- src/pages/PerfilPsic/perfilPsic.jsx
+++ src/pages/PerfilPsic/perfilPsic.jsx
@@ -7,22 +7,16 @@ import check from "../../utils/assets/check-icon.png"
 import { useParams } from "react-router-dom";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import profilePic from "../../utils/assets/sem-imagem-avatar.png"
-import background from "../../utils/assets/vecteezy_modern-abstract-white-and-gray-gradient-background-with_10405766.jpg"
 import api from "../../api";
 
 const PerfilPsic = () => {
-    const centralize = () => {
-        window.scrollTo(0, 0);
-    }
-
     const { id } = useParams();
     const [psicologo, setPsicologo] = useState(null);
 
     const notify = () => toast.success("Agendamento solicitado com sucesso!");
 
     useEffect(() => {
-        api.get(`psicologos/${id}`).then((response) => {
+        api.get(`/${id}`).then((response) => {
             setPsicologo(response.data);
         });
     }, [id]);
@@ -30,16 +24,16 @@ const PerfilPsic = () => {
     console.log(psicologo);
 
     return (
-        <div className={styles['body']} onLoad={centralize}>
-            <NavBar />
+        <div className={styles['body']}>
+            <NavBar/>
             <div className={styles['fundo-container']}>
-                <img src={psicologo?.fundo || background} alt="Avatar" className={styles['img-fundo']} />
+                <img src={psicologo?.fundo} alt="Avatar" className={styles['img-fundo']} />
             </div>
             <div className={styles['info-container']}>
 
                 <div className={styles["header"]}>
                     <div className={styles['img-container']}>
-                        <img src={psicologo?.fotoPerfil || profilePic} alt="Avatar" className={styles['img']} />
+                        <img src={psicologo?.imagemUrl} alt="Avatar" className={styles['img']} />
                     </div>
 
                     <div className={styles["nome"]}>
@@ -51,36 +45,33 @@ const PerfilPsic = () => {
                             </div>
 
                             <div className={styles['atendimentos']}>
-                                <img src={check} alt="Ícone de Check" /><p><b>{psicologo?.qtdAtendimentos}</b> Atendimentos</p>
+                                <img src={check} alt="Ícone de Check" /><p><b>{psicologo?.qntAtendimentos}</b> Atendimentos</p>
                             </div>
                         </div>
-                        <p className={styles['espec']}>{psicologo?.especialidade || "Atendimento Clínico"}</p>
+                        <p className={styles['espec']}>{psicologo?.espec}</p>
                     </div>
-                    <div className={styles["crpEstado"]}>
-                        <p>CRP: {psicologo?.crp}</p>
-                        <p>{psicologo?.estado}|SP</p>
+
+                    <p>CRP: {psicologo?.crp}</p>
+                    <p>{psicologo?.estado}|SP</p>
+                </div>
+                <div className={styles['buttonSobre']}>
+                    <div>
+                        <button className={styles['bntAgend']} onClick={notify}>Solicitar Agendamento</button>
                     </div>
-                </div>
-                <div>
-                    <button className={styles['bntAgend']} onClick={notify}>Solicitar Agendamento</button>
-                </div>
-                {/* <div className={styles['buttonSobre']}> */}
-                <div className={styles['sobre']}>
-                    <h2>Sobre</h2>
-                    <p>{psicologo?.descricao}</p>
-                    {/* </div> */}
+                    <div className={styles['sobre']}>
+                        <h2>Sobre</h2>
+                        <p>{psicologo?.descricao}</p>
+                    </div>
                 </div>
                 <div className={styles['espForm']}>
                     <div className={styles['especialidade']}>
                         <h2>Especialidades</h2>
-                        <br />
-                        <p>{psicologo?.especialidade[1] || "Atendimento Clínico"}</p>
+                        <p>{psicologo?.espec}</p>
                     </div>
                     <div className={styles['formacao']}>
                         <h2>Formação</h2>
-                        <br/>
-                        <h3>{psicologo?.experienciasFormacoes[0] || "Faculdade"}</h3>
-                        <p>{psicologo?.experienciasFormacoes[1] || "Bacharelado"}</p>
+                        {/* <p>{psicologo?.formacao}</p> */}
+                        <p>SPTech</p>
                     </div>
                 </div>
             </div>
