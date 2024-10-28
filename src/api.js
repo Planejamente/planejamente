6bf75f0abac833ae4164cb7d51143f078cb872e3
Icaro Bezerra <icaro.bezerra@sptech.school>
2024-10-28 03:27:17 -0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,9 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
 // baseURL: "http://localhost:8080",
-    baseURL: "https://api.grayfield-6cf3de9d.brazilsouth.azurecontainerapps.io",
-    // baseURL: "https://664b529d35bbda10987c7584.mockapi.io/api/psicologos/psico-user"
-
+    baseURL: process.env.REACT_APP_BASE_URL,
 });
 
 export default api;
