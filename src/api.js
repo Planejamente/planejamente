ffaf03dd83c9dff57aa2774dcfbd7ed4ae7126fe
Icaro Bezerra <icaro.bezerra@sptech.school>
2024-12-09 19:41:49 -0300

    asim

--- src/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-// baseURL: "http://localhost:8080",
-    baseURL: process.env.REACT_APP_BASE_URL,
-});
-
-export default api;
--- src/components/ButtonMod/ButtonMod.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-import styles from "./ButtonMod.module.css";
-
-const ButtonMod = ({ onClick }) => {
